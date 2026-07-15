854b92fdaf848c4e22a0078250cd7785fc2997b2
Lalao RAKOTOMAVO <darirarakotomavo@gmail.com>
2026-07-15 12:16:01 +0300

    Modifications reactconfig.ts

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,7 +1,11 @@
 import type { Config } from "@react-router/dev/config";
+// @ts-ignore - Le module est résolu par Vercel lors du build mais absent en local
+import { vercelPreset } from "@react-router/dev/vercel";
 
 export default {
   // Config options...
-  // Server-side render by default, to enable SPA mode set this to `false`
   ssr: true,
+  
+  // Utilisation directe du preset maintenant que TypeScript ignore l'import
+  presets: [vercelPreset()],
 } satisfies Config;
